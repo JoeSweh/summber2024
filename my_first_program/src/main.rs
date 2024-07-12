71caee30fb3ac28dd99da61202f6bbe89531c445
JoeSweh <123581281+JoeSweh@users.noreply.github.com>
2024-07-12 21:24:27 +0000

    setup

---
+++ my_first_program/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, Jose! To the Moon");
+}
---
+++ my_first_program/target/debug/deps/my_first_program-e842094d59a842fa.d
@@ -0,0 +1,5 @@
+/workspaces/summber2024/my_first_program/target/debug/deps/my_first_program-e842094d59a842fa: src/main.rs
+
+/workspaces/summber2024/my_first_program/target/debug/deps/my_first_program-e842094d59a842fa.d: src/main.rs
+
+src/main.rs:
